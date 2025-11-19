9c1c5af794c3b7ac59c326f4d7d7b8700dcd10ac
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2025-11-19 15:05:35 -0300

    Melhoramentos na interface CSS e JS
    mudanças de margens, pokemoves com grid e com cor junto com mudanças mínimas no JS

--- POKEDEX/script.js
+++ POKEDEX/script.js
@@ -16,20 +16,19 @@ async function randomificar() {
   }
 
   const dados = await resposta.json();
-  document.getElementById("pokenome").textContent = dados.name;
+  document.getElementById("pokenome").textContent = dados.name.toUpperCase();
   document.getElementById("pokeimg").src = dados.sprites.front_default;
-  document.getElementById("poketipo").textContent =
-    "Tipo: " + dados.types[0].type.name;
-  document.getElementById("pokemove1").textContent = dados.moves[0].move.name;
-  document.getElementById("pokemove2").textContent = dados.moves[5].move.name;
-  document.getElementById("pokemove3").textContent = dados.moves[6].move.name;
-  document.getElementById("pokemove4").textContent = dados.moves[7].move.name;
+  document.getElementById("poketipo").textContent ="Tipo: " + dados.types[0].type.name.toUpperCase();
+  document.getElementById("pokemove1").textContent = dados.moves[0].move.name.toUpperCase();
+  document.getElementById("pokemove2").textContent = dados.moves[5].move.name.toUpperCase();
+  document.getElementById("pokemove3").textContent = dados.moves[6].move.name.toUpperCase();
+  document.getElementById("pokemove4").textContent = dados.moves[7].move.name.toUpperCase();
 }
 async function buscar() {
   const nome = document.getElementById("nome").value.toLowerCase();
   const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
   if (!resposta.ok) {
-    document.getElementById("pokenome").textContent = "Pokemon não encontrado!";
+    document.getElementById("pokenome").textContent = "POKEMON NÃO ENCONTRADO";
     document.getElementById("pokeimg").src = "";
     document.getElementById("poketipo").textContent = "";
     document.getElementById("pokemoves1").textContent = "";
@@ -40,12 +39,11 @@ async function buscar() {
   }
 
   const dados = await resposta.json();
-  document.getElementById("pokenome").textContent = dados.name;
+  document.getElementById("pokenome").textContent = dados.name.toUpperCase();
   document.getElementById("pokeimg").src = dados.sprites.front_default;
-  document.getElementById("poketipo").textContent =
-    "Tipo: " + dados.types[0].type.name;
-  document.getElementById("pokemove1").textContent = dados.moves[0].move.name;
-  document.getElementById("pokemove2").textContent = dados.moves[5].move.name;
-  document.getElementById("pokemove3").textContent = dados.moves[6].move.name;
-  document.getElementById("pokemove4").textContent = dados.moves[7].move.name;
+  document.getElementById("poketipo").textContent = "Tipo: " + dados.types[0].type.name.toUpperCase();
+  document.getElementById("pokemove1").textContent = dados.moves[0].move.name.toUpperCase();
+  document.getElementById("pokemove2").textContent = dados.moves[5].move.name.toUpperCase();
+  document.getElementById("pokemove3").textContent = dados.moves[6].move.name.toUpperCase();
+  document.getElementById("pokemove4").textContent = dados.moves[7].move.name.toUpperCase();
 }
