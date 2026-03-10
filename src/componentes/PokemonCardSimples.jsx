603ebb9c7cfd6e5228719ab17ec186c5e9ea20c0
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2026-03-10 17:55:45 -0300

    Novas funcionalidades - botão shiny
    Deixa o pokemon shiny quando clicado

--- src/componentes/PokemonCardSimples.jsx
+++ src/componentes/PokemonCardSimples.jsx
@@ -1,6 +1,16 @@
 import "./componentes.css";
+import { useState } from "react";
 
 export function PokemonCardSimples(props) {
+
+  const [shiny, setShiny] = useState(null)
+  const imagemAtual = shiny !== null ? props.imagemShiny : props.imagem
+
+    function deixarShiny(){
+      const ehShiny = shiny === null ? setShiny(props.imagemShiny) : setShiny(null)
+      return ehShiny;
+    }
+
   return (
     <section>
       <div className="pokemon">
@@ -14,21 +24,24 @@ export function PokemonCardSimples(props) {
           <p>EXP: {props.experiencia}</p>
         </div>
 
-        <img src={props.imagem} alt={props.nome} className="pokeImagem"/>
+        <img src={imagemAtual} alt={props.nome} className="pokeImagem"/>
 
         <div className="info">
           <p>Id: #{props.numero}</p>
           <p>Altura: {props.altura}</p>
           <p>Peso: {props.peso}</p>
-          <button>shiny</button>
+          <br />
+          <button className="botaoShiny" onClick={deixarShiny}>
+            {shiny ? "✨ NORMAL ✨" : "✨ SHINY ✨"}</button>
         </div>
 
         <hr></hr>
         <div className="habilidades">
           <p className="pokeHabilidade">{props.habilidadeUm}</p>
-          <p className="pokeHabilidade">{props.habilidadeDois}</p>
-          <p className="pokeHabilidade">{props.habilidadeUm}</p>
-          <p className="pokeHabilidade">{props.habilidadeDois}</p>
+          {props.habilidadeDois !== "" && (<p className="pokeHabilidade">{props.habilidadeDois}</p>)}
+          {props.habilidadeTres !== "" && (<p className="pokeHabilidade">{props.habilidadeTres}</p>)}
+          {props.habilidadeQuatro !== "" && (<p className="pokeHabilidade">{props.habilidadeQuatro}</p>)}
+          
         </div>
       </div>
     </section>
--- src/componentes/PokemonContainer.jsx
+++ src/componentes/PokemonContainer.jsx
@@ -1,31 +1,42 @@
-import { useEffect, useState } from 'react'
-import './componentes.css'
-import { PokemonCardSimples } from './PokemonCardSimples';
+import { useEffect, useState } from "react";
+import "./componentes.css";
+import { PokemonCardSimples } from "./PokemonCardSimples";
 
-export function PokemonContainer(){
-    const [pokemon, setPokemon]=useState(null)
+export function PokemonContainer() {
+  const [pokemon, setPokemon] = useState(null);
 
-    useEffect(()=>{
-        fetch("https://pokeapi.co/api/v2/pokemon/pikachu").then((resposta)=>resposta.json()).then((dados)=>{
-            setPokemon(dados)
-        });
-    },[]);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setPokemon(dados);
+      });
+  }, []);
 
-    if(!pokemon){
-        return <p>Carregando pokemon. . .</p>
-    }
-    return(
-        <PokemonCardSimples
-        nome={pokemon.name}
-        imagem={pokemon.sprites.front_default}
-        numero={pokemon.id}
-        altura={pokemon.height}
-        peso={pokemon.weight}
-        tipoUm={pokemon.types[0].type.name}
-        tipoDois={pokemon.types[1] ? " / "+pokemon.types[1].type.name : "" }
-        habilidadeUm={pokemon.abilities[0].ability.name}
-        habilidadeDois={pokemon.abilities[1] ? pokemon.abilities[1].ability.name : ""}
-        experiencia={pokemon.base_experience}
-        />
-    );
+  if (!pokemon) {
+    return <p>Carregando pokemon. . .</p>;
+  }
+  return (
+    <PokemonCardSimples
+      nome={pokemon.name}
+      imagem={pokemon.sprites.front_default}
+      imagemShiny={pokemon.sprites.front_shiny}
+      numero={pokemon.id}
+      altura={pokemon.height}
+      peso={pokemon.weight}
+      tipoUm={pokemon.types[0].type.name}
+      tipoDois={pokemon.types[1] ? " / " + pokemon.types[1].type.name : ""}
+      habilidadeUm={pokemon.abilities[0].ability.name}
+      habilidadeDois={
+        pokemon.abilities[1] ? pokemon.abilities[1].ability.name : ""
+      }
+      habilidadeTres={
+        pokemon.abilities[2] ? pokemon.abilities[2].ability.name : ""
+      }
+      habilidadeQuatro={
+        pokemon.abilities[3] ? pokemon.abilities[3].ability.name : ""
+      }
+      experiencia={pokemon.base_experience}
+    />
+  );
 }
